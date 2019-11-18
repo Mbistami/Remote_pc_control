328fb3de42cd89dec00a0906cf05f37724e2f5a7
Mbistami <42296438+Mbistami@users.noreply.github.com>
2019-11-18 03:04:07 +0000

    1st

---
+++ Botme/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Botme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gunaDragControl1 = new Guna.UI.WinForms.GunaDragControl(this.components);
+            this.gunaElipsePanel1 = new Guna.UI.WinForms.GunaElipsePanel();
+            this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaTextBox1 = new Guna.UI.WinForms.GunaTextBox();
+            this.gunaButton1 = new Guna.UI.WinForms.GunaButton();
+            this.gunaLabel2 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel3 = new Guna.UI.WinForms.GunaLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.gunaElipsePanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gunaDragControl1
+            // 
+            this.gunaDragControl1.TargetControl = this;
+            // 
+            // gunaElipsePanel1
+            // 
+            this.gunaElipsePanel1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaElipsePanel1.BaseColor = System.Drawing.Color.RoyalBlue;
+            this.gunaElipsePanel1.Controls.Add(this.gunaLabel3);
+            this.gunaElipsePanel1.Controls.Add(this.gunaLabel2);
+            this.gunaElipsePanel1.Controls.Add(this.gunaLabel1);
+            this.gunaElipsePanel1.Controls.Add(this.gunaTextBox1);
+            this.gunaElipsePanel1.Location = new System.Drawing.Point(12, 12);
+            this.gunaElipsePanel1.Name = "gunaElipsePanel1";
+            this.gunaElipsePanel1.Size = new System.Drawing.Size(235, 95);
+            this.gunaElipsePanel1.TabIndex = 0;
+            // 
+            // gunaLabel1
+            // 
+            this.gunaLabel1.AutoSize = true;
+            this.gunaLabel1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaLabel1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.gunaLabel1.Location = new System.Drawing.Point(79, 6);
+            this.gunaLabel1.Name = "gunaLabel1";
+            this.gunaLabel1.Size = new System.Drawing.Size(64, 15);
+            this.gunaLabel1.TabIndex = 1;
+            this.gunaLabel1.Text = "Your Email";
+            this.gunaLabel1.Click += new System.EventHandler(this.gunaLabel1_Click);
+            // 
+            // gunaTextBox1
+            // 
+            this.gunaTextBox1.BaseColor = System.Drawing.Color.White;
+            this.gunaTextBox1.BorderColor = System.Drawing.Color.Silver;
+            this.gunaTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.gunaTextBox1.FocusedBaseColor = System.Drawing.Color.White;
+            this.gunaTextBox1.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaTextBox1.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.gunaTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaTextBox1.Location = new System.Drawing.Point(40, 24);
+            this.gunaTextBox1.Name = "gunaTextBox1";
+            this.gunaTextBox1.PasswordChar = '\0';
+            this.gunaTextBox1.Size = new System.Drawing.Size(160, 30);
+            this.gunaTextBox1.TabIndex = 0;
+            this.gunaTextBox1.TextChanged += new System.EventHandler(this.gunaTextBox1_TextChanged);
+            // 
+            // gunaButton1
+            // 
+            this.gunaButton1.AnimationHoverSpeed = 0.07F;
+            this.gunaButton1.AnimationSpeed = 0.03F;
+            this.gunaButton1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaButton1.BorderColor = System.Drawing.Color.Black;
+            this.gunaButton1.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.gunaButton1.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaButton1.ForeColor = System.Drawing.Color.White;
+            this.gunaButton1.Image = null;
+            this.gunaButton1.ImageSize = new System.Drawing.Size(20, 20);
+            this.gunaButton1.Location = new System.Drawing.Point(52, 126);
+            this.gunaButton1.Name = "gunaButton1";
+            this.gunaButton1.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.gunaButton1.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaButton1.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaButton1.OnHoverImage = null;
+            this.gunaButton1.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaButton1.Size = new System.Drawing.Size(160, 42);
+            this.gunaButton1.TabIndex = 1;
+            this.gunaButton1.Text = "Start";
+            this.gunaButton1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.gunaButton1.Click += new System.EventHandler(this.gunaButton1_Click);
+            // 
+            // gunaLabel2
+            // 
+            this.gunaLabel2.AutoSize = true;
+            this.gunaLabel2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaLabel2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.gunaLabel2.Location = new System.Drawing.Point(3, 69);
+            this.gunaLabel2.Name = "gunaLabel2";
+            this.gunaLabel2.Size = new System.Drawing.Size(80, 15);
+            this.gunaLabel2.TabIndex = 2;
+            this.gunaLabel2.Text = "Botme Email :";
+            // 
+            // gunaLabel3
+            // 
+            this.gunaLabel3.AutoSize = true;
+            this.gunaLabel3.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaLabel3.ForeColor = System.Drawing.Color.Orange;
+            this.gunaLabel3.Location = new System.Drawing.Point(81, 69);
+            this.gunaLabel3.Name = "gunaLabel3";
+            this.gunaLabel3.Size = new System.Drawing.Size(64, 15);
+            this.gunaLabel3.TabIndex = 3;
+            this.gunaLabel3.Text = "Your Email";
+            this.gunaLabel3.Click += new System.EventHandler(this.gunaLabel3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(259, 184);
+            this.Controls.Add(this.gunaButton1);
+            this.Controls.Add(this.gunaElipsePanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "BotMe";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gunaElipsePanel1.ResumeLayout(false);
+            this.gunaElipsePanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI.WinForms.GunaDragControl gunaDragControl1;
+        private Guna.UI.WinForms.GunaButton gunaButton1;
+        private Guna.UI.WinForms.GunaElipsePanel gunaElipsePanel1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel1;
+        private Guna.UI.WinForms.GunaTextBox gunaTextBox1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel3;
+        private Guna.UI.WinForms.GunaLabel gunaLabel2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Botme/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace Botme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        GuerrillaMail Mail = new GuerrillaMail();
+        GuerrillaMail.Email reply = new GuerrillaMail.Email();
+        int i = 0;
+
+        private void gunaLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gunaTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void sender()
+        {
+            
+            MailMessage mail = new MailMessage();
+            System.Net.Mail.SmtpClient smtpServer = new System.Net.Mail.SmtpClient("smtp.gmail.com");
+            Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, Screen.PrimaryScreen.Bounds.Size);
+                bmp.Save("Screenshot" + i + ".png");  // saves the image
+            }
+            mail.From = new MailAddress("botme.boter@gmail.com");
+            mail.To.Add(gunaTextBox1.Text);
+            mail.Subject = "Bot state";
+
+
+
+            System.Net.Mail.Attachment attachment = new System.Net.Mail.Attachment("Screenshot" + i + ".png");
+            attachment.ContentDisposition.Inline = false;
+            mail.Body = string.Format("Hey" + @"<img src=""cid:{0}"" />", attachment.ContentId);
+            smtpServer.Port = 587;
+            smtpServer.Credentials = new System.Net.NetworkCredential("botme.boter@gmail.com", "a642705MOUSSABTM");
+            smtpServer.EnableSsl = true;
+            mail.IsBodyHtml = true;
+            mail.Attachments.Add(attachment);
+            smtpServer.Send(mail);
+            //File.Delete("Screenshot.png");
+            MessageBox.Show("Sent");
+            i++;
+            for (int j = 0; j < i; j++)
+            {
+                File.Delete("Screenshot" + j + ".png");
+                MessageBox.Show("sss");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gunaLabel3.Text = Mail.GetMyEmail().ToString();
+        }
+
+        private void gunaButton1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void gunaLabel3_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(gunaLabel3.Text);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Mail.GetLastEmail() != null)
+            {
+                var check = Mail.GetLastEmail().mail_subject.ToLower();
+                var uqid = Mail.GetLastEmail().mail_id.ToString();
+                if (check.Equals("botmehello"))
+                {
+                    Mail.DeleteSingleEmail(uqid);
+                    MessageBox.Show("Done");
+                    this.sender();
+                }
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Stop();
+            
+        }
+    }
+}
